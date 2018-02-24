4ce10e9fb915bb0b3ebe156da710f484d2ccacbd
Faisal Khan <faisalkhan@techgeek.co.in>
2018-02-24 14:29:56 -0800

    Initial changes for the exical Analysis

---
+++ src/com/compiler/base/Lexer323.java
@@ -0,0 +1,11 @@
+package com.compiler.base;
+
+public class Lexer323 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input = "Get the work Done + //";
+		
+	}
+
+}
---
+++ src/com/compiler/base/Token.java
@@ -0,0 +1,12 @@
+package com.compiler.base;
+
+public class Token {
+	private String token;
+	private String data;
+
+	public Token(String token, String data) {
+		super();
+		this.token = token;
+		this.data = data;
+	}
+}
